c331629ee966cefbf65436fb64159786c4134c33
viveksinra <vivek.kr.vks@gmail.com>
2025-09-25 20:20:46 +0530

    Refine Header button styling for improved hover effects and update Section3 layout to enhance grid structure for BenefitCards, promoting better user engagement.

--- components/Home/Section3.tsx
+++ components/Home/Section3.tsx
@@ -9,7 +9,7 @@ export default function Section3() {
           What You&apos;ll Gain by Attending
         </h2>
 
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 lg:grid-cols-4">
           <BenefitCard
             imageSrc="/assets/images/home/discover.png"
             title={"Discover Career-\nCosting Mistakes"}
